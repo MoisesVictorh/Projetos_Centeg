56d6f9bbc504d6c607d38ccd5356da84b46fc0ad
Moises Victor <moisesvictorh16@gmail.com>
2026-03-19 20:59:11 -0300

    Número maior e menor

---
+++ maiormenor/Program.vb
@@ -0,0 +1,39 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim num1 as Integer  
+        Dim num2 as Integer  
+
+        Console.WriteLine("Me informe o primeiro número:")
+
+        num1 = console.readline()
+
+        Console.WriteLine("Agora me informe o segundo número:")
+
+        num2 = console.readline()
+
+        if num1 > num2
+
+        console.writeline (num1 & " é maior que " & num2)
+
+        else if num1 < num2
+
+            console.writeline (num2 & " é maior que " & num1)
+
+        Else
+
+            Console.writeline ("Os números são iguais")
+
+        end if  
+
+
+
+
+
+
+
+
+    End Sub
+End Module
