2b70119409914f1a180670dbd48d5f49170a654d
Moises Victor <moisesvictorh16@gmail.com>
2026-03-24 20:43:30 -0300

    Vetor exercicio

---
+++ sistemas/Program.vb
@@ -0,0 +1,20 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim sistemas(3) as string
+
+        sistemas(0) = "Windows"
+        sistemas(1) = "Linux"
+        sistemas(2) = "IOS"
+
+        Console.writeline("Os sitemas operacionais são:")
+        Console.writeline(sistemas(0))
+        console.writeline(sistemas(1))
+        console.writeline(sistemas(2))
+
+        
+
+    End Sub
+End Module
