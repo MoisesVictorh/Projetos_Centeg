525a2342b4263cc8ec692dc2311a8161a5a19508
Moises Victor <moisesvictorh16@gmail.com>
2026-03-24 20:18:47 -0300

    Vetor

---
+++ Aula24032026/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim nomesobrenome(2) as String
+
+        Console.writeline("Digite seu nome completo:")
+
+        nomesobrenome(0) = "Moises"
+        nomesobrenome(1) = "Victor"
+
+        Console.writeline("Seu nome seria " & nomesobrenome(0)  &  nomesobrenome(1))
+
+    End Sub
+End Module
