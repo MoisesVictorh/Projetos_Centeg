d2820f27b09fb04b6abd37705b7d0d5c22fe10c5
Moises Victor <moisesvictorh16@gmail.com>
2026-03-26 21:00:13 -0300

    deploy contabilizer

---
+++ Arrays/Program.vb
@@ -0,0 +1,18 @@
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Dim contabilizer as Integer
+         
+
+        for contabilizer = 1 to 10 
+         console.writeline("5 x " & contabilizer = & * 5)
+        next
+
+    
+
+    End Sub
+End Module
+
